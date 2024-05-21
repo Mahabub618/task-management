a67428149fb07000d412fa58193aaa1d79eff141
Mahabub Rahman <mahabub@stud.cou.ac.bd>
2024-05-22 00:28:47 +0600

    Setup configuration in module, jwt strategy and typeorm config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,13 +5,17 @@ import { JwtPayload } from './jwt-payload.interface';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import * as config from 'config'
+import * as process from 'process';
+
+const jwtConfig = config.get('jwt');
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(@InjectRepository(User) private userRepository: Repository<User>) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'topSecret51'
+      secretOrKey: process.env.JWT_SECRET || jwtConfig.secret
     });
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config'
+import * as process from 'process';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const app = await NestFactory.create(AppModule);
 
-  const serverConfig = config.get('server');
-  await app.listen(3000);
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
 }
 bootstrap();
