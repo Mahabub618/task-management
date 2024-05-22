c1aed9bb2b1137fbc6c46f08b2279eb223a5ee19
Mahabub Rahman <mahabub@stud.cou.ac.bd>
2024-05-22 17:09:19 +0600

    Set configuration to CORS enabling for both development and production

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import * as config from 'config'
 import * as process from 'process';
 
 async function bootstrap() {
-  const serverConfig = config.get('server');
   const app = await NestFactory.create(AppModule);
+  const serverConfig = config.get('server');
+  if (process.env.NODE_ENV == 'development') {
+    app.enableCors();
+  }
+  else {
+    app.enableCors({origin: serverConfig.origin});
+  }
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
