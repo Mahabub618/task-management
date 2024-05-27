e577a404cee722e2172b6f1dfb58625efab59291
Mahabub Rahman <mahabub@stud.cou.ac.bd>
2024-05-27 21:52:53 +0600

    Update app according to latest development

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,17 +5,18 @@ import { JwtPayload } from './jwt-payload.interface';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
-import * as config from 'config'
-import * as process from 'process';
+import { ConfigService } from '@nestjs/config';
 
-const jwtConfig = config.get('jwt');
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(@InjectRepository(User) private userRepository: Repository<User>) {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private configService: ConfigService
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET || jwtConfig.secret
+      secretOrKey: configService.get('JWT_SECRET')
     });
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as config from 'config'
 import * as process from 'process';
+import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const serverConfig = config.get('server');
-  if (process.env.NODE_ENV == 'development') {
-    app.enableCors();
-  }
-  else {
-    app.enableCors({origin: serverConfig.origin});
-  }
-
-  const port = process.env.PORT || serverConfig.port;
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  const port = process.env.PORT;
   await app.listen(port);
 }
 bootstrap();
